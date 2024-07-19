f50c3e890fb792b4fa10157eba11a51418322fa7
Chen Meng <78586843+slmpc@users.noreply.github.com>
2024-07-19 23:57:29 +0800

    Added Render2DEvent

---
+++ src/main/java/dev/exceptionteam/sakura/mixins/gui/InGameHudMixin.java
@@ -0,0 +1,32 @@
+package dev.exceptionteam.sakura.mixins.gui;
+
+import dev.exceptionteam.sakura.events.impl.Render2DEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        client.getProfiler().push("SakuraRender2D");
+
+        Render2DEvent event = new Render2DEvent(context, tickCounter.getTickDelta(false));
+        event.post();
+
+        client.getProfiler().pop();
+    }
+
+}
---
+++ src/main/kotlin/dev/exceptionteam/sakura/events/impl/Render2DEvent.kt
@@ -0,0 +1,9 @@
+package dev.exceptionteam.sakura.events.impl
+
+import dev.exceptionteam.sakura.events.Event
+import net.minecraft.client.gui.DrawContext
+
+class Render2DEvent(
+    val context: DrawContext,
+    val tickDelta: Float
+): Event()
