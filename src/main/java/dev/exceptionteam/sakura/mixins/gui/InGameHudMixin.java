093d81c9618101dffb03b42a30922c1ad1490298
Chen Meng <78586843+slmpc@users.noreply.github.com>
2024-10-25 16:12:30 +0800

    feat: Updated 1.21.3

--- src/main/java/dev/exceptionteam/sakura/mixins/gui/InGameHudMixin.java
+++ src/main/java/dev/exceptionteam/sakura/mixins/gui/InGameHudMixin.java
@@ -1,13 +1,12 @@
 package dev.exceptionteam.sakura.mixins.gui;
 
 import dev.exceptionteam.sakura.graphics.RenderSystem;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.profiler.Profilers;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,17 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
-    @Shadow
-    @Final
-    private MinecraftClient client;
-
     @Inject(method = "render", at = @At("TAIL"))
     private void onRender(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        client.getProfiler().push("SakuraRender2D");
+
+        Profiler profiler = Profilers.get();
+        profiler.push("sakura-render2d");
 
         RenderSystem.INSTANCE.onRender2d(context);
 
-        client.getProfiler().pop();
+        profiler.pop();
     }
 
 }
