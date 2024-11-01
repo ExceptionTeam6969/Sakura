b61881389aacd307d456d0b3b8f6f2d80a2a0a84
Chen Meng <78586843+slmpc@users.noreply.github.com>
2024-11-01 21:48:29 +0800

    chore: Updated build script of gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,17 +44,6 @@ tasks {
     }
 
     compileKotlin {
-        compilerOptions {
-            freeCompilerArgs.addAll(
-                "-opt-in=kotlin.RequiresOptIn",
-                "-opt-in=kotlin.contracts.ExperimentalContracts",
-                "-Xlambdas=indy",
-                "-Xjvm-default=all",
-                "-Xbackend-threads=0",
-                "-Xno-source-debug-extension",
-                "-Xcontext-receivers"
-            )
-        }
     }
 
     processResources {
