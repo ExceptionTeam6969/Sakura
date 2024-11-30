148bd6f51144befdeda203cc0e12fb0b5837dcc0
Chen Meng <miaoyuxuan10@outlook.com>
2024-11-30 12:32:59 +0800

    cleanup: Code cleanup

--- src/main/kotlin/dev/exceptionteam/sakura/utils/memory/Memory.kt
+++ src/main/kotlin/dev/exceptionteam/sakura/utils/memory/Memory.kt
@@ -1,10 +1,7 @@
 package dev.exceptionteam.sakura.utils.memory
 
-import org.lwjgl.system.MemoryStack
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
-
-val memStack get() = MemoryStack.stackPush()
 
 fun createDirectByteBuffer(capacity: Int): ByteBuffer {
     return ByteBuffer.allocateDirect(capacity).order(ByteOrder.nativeOrder())
--- src/main/kotlin/dev/exceptionteam/sakura/utils/world/WorldUtils.kt
+++ src/main/kotlin/dev/exceptionteam/sakura/utils/world/WorldUtils.kt
@@ -2,9 +2,11 @@ package dev.exceptionteam.sakura.utils.world
 
 import net.minecraft.client.Minecraft
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.block.state.BlockState
 
 object WorldUtils {
 
-    val BlockPos.blockState get() = Minecraft.getInstance().level!!.getBlockState(this)
+    val BlockPos.blockState: BlockState get() =
+        Minecraft.getInstance().level!!.getBlockState(this)
 
 }
