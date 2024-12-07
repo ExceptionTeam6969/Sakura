68f6141f1ee20610f4261c97cfffa4ba9a0fd7d1
Chen Meng <miaoyuxuan10@outlook.com>
2024-12-07 16:25:34 +0800

    cleanup: Code cleanup

--- src/main/kotlin/dev/exceptionteam/sakura/Sakura.kt
+++ src/main/kotlin/dev/exceptionteam/sakura/Sakura.kt
@@ -2,11 +2,11 @@ package dev.exceptionteam.sakura
 
 import dev.exceptionteam.sakura.managers.Managers
 import org.apache.logging.log4j.LogManager
-import org.lwjgl.glfw.GLFW
+import org.apache.logging.log4j.Logger
 
 object Sakura {
 
-    val logger = LogManager.getLogger("Sakura")
+    val logger: Logger = LogManager.getLogger("Sakura")
 
     const val NAME = "Sakura"
     const val VERSION = "1.0"
