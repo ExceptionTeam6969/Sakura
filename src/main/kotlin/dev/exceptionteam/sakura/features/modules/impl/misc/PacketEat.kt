fc0c6aed05943aac654c8cc5e6c245e7860b09f0
Chen Meng <miaoyuxuan10@outlook.com>
2025-01-11 16:59:41 +0800

    refactor: Refactored packet eat

--- src/main/kotlin/dev/exceptionteam/sakura/features/modules/impl/misc/PacketEat.kt
+++ src/main/kotlin/dev/exceptionteam/sakura/features/modules/impl/misc/PacketEat.kt
@@ -6,23 +6,19 @@ import dev.exceptionteam.sakura.features.modules.Category
 import dev.exceptionteam.sakura.features.modules.Module
 import net.minecraft.core.component.DataComponents
 import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket
-import net.minecraft.world.food.FoodProperties
-import net.minecraft.world.item.ItemStack
 
 object PacketEat: Module(
-    name = "PacketEat",
+    name = "packet-eat",
     category = Category.MISC,
 ) {
     init {
         nonNullListener<PacketEvents.Send> { e ->
-            if (mc.player != null) {
-                if (e.packet is ServerboundPlayerActionPacket && e.packet.action === ServerboundPlayerActionPacket.Action.RELEASE_USE_ITEM) {
-                    val activeItem: ItemStack = player.useItem
-                    val foodComponent: FoodProperties? = activeItem.item.components().get(DataComponents.FOOD)
-                    if (foodComponent != null) {
-                        e.cancel()
-                    }
-                }
+            if (e.packet !is ServerboundPlayerActionPacket) return@nonNullListener
+
+            if (e.packet.action != ServerboundPlayerActionPacket.Action.RELEASE_USE_ITEM) return@nonNullListener
+
+            player.useItem.item.components().get(DataComponents.FOOD)?.let {
+                e.cancel()
             }
         }
     }
