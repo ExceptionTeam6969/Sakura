2c75c4be402edd05c7dbf1f45c2023f7003573a0
Chen Meng <miaoyuxuan10@outlook.com>
2025-01-18 19:05:25 +0800

    fix: Fix pearl clip can't run on grim ac server

--- src/main/kotlin/dev/exceptionteam/sakura/features/modules/impl/combat/PearlClip.kt
+++ src/main/kotlin/dev/exceptionteam/sakura/features/modules/impl/combat/PearlClip.kt
@@ -12,7 +12,6 @@ import dev.exceptionteam.sakura.utils.player.InventoryUtils.findItemInHotbar
 import net.minecraft.network.protocol.game.ServerboundUseItemPacket
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.item.Items
-import net.minecraft.world.phys.Vec3
 
 object PearlClip: Module(
     name = "pearl-clip",
@@ -22,7 +21,17 @@ object PearlClip: Module(
     private val pitch by setting("pitch", 80f, 0f..90f)
     private val switchMode by setting("switch-mode", SwitchMode.PICK)
 
+    private var secondTickFlag = false
+
     init {
+
+        onEnable {
+            secondTickFlag = false
+        }
+
+        onDisable {
+            secondTickFlag = false
+        }
 
         nonNullListener<TickEvent.Update> {
             if (switchMode == SwitchMode.OFF && player.mainHandItem.item != Items.ENDER_PEARL) {
@@ -30,19 +39,24 @@ object PearlClip: Module(
                 return@nonNullListener
             }
 
-            addRotation(player.yRot, pitch, 100) {
-                val slot = findItemInHotbar(Items.ENDER_PEARL) ?: run {
-                    ChatUtils.sendWarnMessage("Can't find ender pearl in hotbar! disabling...")
-                    return@addRotation
-                }
+            if (secondTickFlag) {
+                addRotation(player.yRot, pitch, 100) {
+                    val slot = findItemInHotbar(Items.ENDER_PEARL) ?: run {
+                        ChatUtils.sendWarnMessage("Can't find ender pearl in hotbar! disabling...")
+                        return@addRotation
+                    }
 
-                switch(switchMode, slot) {
-                    player.deltaMovement = Vec3.ZERO    // Reset movement
-                    connection.send(ServerboundUseItemPacket(InteractionHand.MAIN_HAND, 0, player.yRot, pitch))
+                    switch(switchMode, slot) {
+                        connection.send(ServerboundUseItemPacket(InteractionHand.MAIN_HAND, 0, player.yRot, pitch))
+                    }
                 }
+                secondTickFlag = false
+                disable()
+            } else {
+                addRotation(player.yRot, pitch, 100)
+                secondTickFlag = true
             }
 
-            disable()
         }
 
     }
