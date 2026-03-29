fde42f4ac453d1e8593bf022079773009b0acaa4
L3MonKe <hhcrystal2008@gmail.com>
2026-03-29 13:02:05 +0800

    Update ClickGUIScreen.kt

--- src/main/kotlin/team/exception/sakura/gui/clickgui/ClickGUIScreen.kt
+++ src/main/kotlin/team/exception/sakura/gui/clickgui/ClickGUIScreen.kt
@@ -11,7 +11,7 @@ object ClickGUIScreen: Screen(Component.literal("Sakura-ClickGUI")) {
         super.render(guiGraphics, mouseX, mouseY, partialTick)
     }
 
-    override fun renderBackground(): Boolean = false
+    override fun renderBackground(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {}
     override fun isPauseScreen(): Boolean = false
     override fun shouldCloseOnEsc(): Boolean = true
     override fun onClose() {
